3be9b3db2a92cd60b115ca1ddd192b888a66fac8
Nathaniel Calderon <nathaniel.calderon@gmail.com>
2021-08-03 00:42:12 -0400

    WIP

--- src/main/kotlin/com/ncalderon/challenges/Arrays.kt
+++ src/main/kotlin/com/ncalderon/challenges/Arrays.kt
@@ -4,16 +4,16 @@ const val max_rows = 6
 const val max_columns = 6
 
 fun hourglassSum(arr: Array<Array<Int>>): Int {
-    var maxHourGlassSum = 0
-    for (i in 0 until max_rows - 4){
-        for (j in 0 until max_columns - 4){
+    var maxHourGlassSum = -46
+    for (i in 0 until max_rows - 2) {
+        for (j in 0 until max_columns - 2) {
             val topIdxRow = i
-            val centerIdxRow: Int = i+1
-            val bottomIdxRow = i+1
+            val centerIdxRow: Int = i + 1
+            val bottomIdxRow = i + 2
 
-            val leftIdxColumn = j+2
-            val centerIdxColumn = j+1
-            val rightIdxColumn = j+2
+            val leftIdxColumn = j
+            val centerIdxColumn = j + 1
+            val rightIdxColumn = j + 2
             var currentHourGlassSum = arr[topIdxRow][leftIdxColumn] + arr[topIdxRow][centerIdxColumn] +
                     arr[topIdxRow][rightIdxColumn]
             currentHourGlassSum += arr[centerIdxRow][centerIdxColumn]
@@ -27,3 +27,51 @@ fun hourglassSum(arr: Array<Array<Int>>): Int {
 
     return maxHourGlassSum
 }
+
+fun rotLeft(a: Array<Int>, d: Int): Array<Int> {
+    val rotatedIndices = (0 until d).toList()
+    val unchangedIndices = (d until a.size).toList()
+    val result = a.sliceArray(unchangedIndices.plus(rotatedIndices))
+    return result
+}
+
+fun minimumBribes(q: Array<Int>): Unit {
+    var bribeCount = 0;
+    var isChaotic = false
+    var current = -1
+    var currentBribeCount = 0;
+    for (i in (q.size-1) downTo 0) {
+        if (current == -1)
+            current = i
+
+        if (q[i] == current+1){
+            currentBribeCount = current-i
+            bribeCount+= currentBribeCount
+            current = -1
+        }
+
+        if (currentBribeCount >= 3){
+            isChaotic = true;
+            break;
+        }
+    }
+
+    if (!isChaotic){
+        bribeCount+=bribeCount
+        println(bribeCount)
+    }
+    else
+        println("Too chaotic")
+}
+
+//        var currentValue = q[i]
+//        if (currentValue == i)
+//            continue
+//        else if (currentValue == i+2)
+//            bribeCount++
+//        else if (currentValue == i+3)
+//            bribeCount+=2
+//        else {
+//            isChaotic = true;
+//            break;
+//        }
--- src/test/kotlin/com/ncalderon/challenges/ArraysKtTest.kt
+++ src/test/kotlin/com/ncalderon/challenges/ArraysKtTest.kt
@@ -1,36 +1,75 @@
 package com.ncalderon.challenges
 
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 internal class ArraysKtTest {
 
     private fun processInput(inputLines: List<String>): Array<Array<Int>> {
         val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
 
-        for (it in inputLines) {
-            for (i in 0 until 6) {
-                arr[i] = it.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
-            }
+        for (i in 0 until 6) {
+            arr[i] = inputLines.get(i).trimEnd().split(" ").map { s -> s.toInt() }.toTypedArray()
         }
-
         return arr
     }
 
     @Test
     fun hourglassSumTest() {
 
-        val inputLines =
-                "1 1 1 0 0 0\n" +
+        var inputLines =
+            "1 1 1 0 0 0\n" +
+                    "0 1 0 0 0 0\n" +
+                    "1 1 1 0 0 0\n" +
+                    "0 0 2 4 4 0\n" +
+                    "0 0 0 2 0 0\n" +
+                    "0 0 1 2 4 0"
+
+        var actual = hourglassSum(processInput(inputLines.split("\n")))
+        assertEquals(19, actual)
+
+        inputLines = "1 1 1 0 0 0\n" +
                 "0 1 0 0 0 0\n" +
                 "1 1 1 0 0 0\n" +
-                "0 0 2 4 4 0\n" +
-                "0 0 0 2 0 0\n" +
+                "0 0 0 0 0 0\n" +
+                "0 0 0 0 0 0\n" +
+                "0 0 0 0 0 0"
+
+        actual = hourglassSum(processInput(inputLines.split("\n")))
+        assertEquals(7, actual)
+
+        inputLines = "-9 -9 -9 1 1 1\n" +
+                "0 -9 0 4 3 2\n" +
+                "-9 -9 -9 1 2 3\n" +
+                "0 0 8 6 6 0\n" +
+                "0 0 0 -2 0 0\n" +
                 "0 0 1 2 4 0"
 
-        hourglassSum(processInput(inputLines.split("\n")))
+        actual = hourglassSum(processInput(inputLines.split("\n")))
+        assertEquals(28, actual)
+
+        inputLines = "-1 -1 0 -9 -2 -2\n" +
+                "-2 -1 -6 -8 -2 -5\n" +
+                "-1 -1 -1 -2 -3 -4\n" +
+                "-1 -9 -2 -4 -4 -5\n" +
+                "-7 -3 -3 -2 -9 -9\n" +
+                "-1 -3 -1 -2 -4 -5"
+
+        actual = hourglassSum(processInput(inputLines.split("\n")))
+        assertEquals(-6, actual)
     }
 
+    @Test
+    fun rotLeftTest(){
+        assertEquals("5 1 2 3 4", rotLeft("1 2 3 4 5".split(" ").map { s -> s.toInt() }.toTypedArray(), 4)
+            .joinToString(" "))
+    }
+
+    @Test
+    fun minimumBribesTest(){
+        minimumBribes(arrayOf(1, 2, 5, 3, 7, 8, 6, 4))
+//        minimumBribes(arrayOf(2, 1, 5, 3, 4))
+//        minimumBribes(arrayOf(2, 5, 1, 3, 4))
+
+    }
 }
